ee49a95fc7e0f583c21c539a8ad379ef665f6beb
JohnPH75 <johnhussain75@gmail.com>
2016-06-15 17:06:09 -0400

    pet cast

--- objc-PetCast/FISAppDelegate.m
+++ objc-PetCast/FISAppDelegate.m
@@ -1,10 +1,41 @@
 //  FISAppDelegate.m
 
 #import "FISAppDelegate.h"
+#import "FISPet.h"
+#import "FISDog.h"
 
 @implementation FISAppDelegate
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+//    FISDog *dogThatCantWoof = [[FISDog alloc]init];
+//    
+//    NSLog(@"%@",[((FISDog *)dogThatCantWoof) makeASound]);
+//    
+//    NSLog(@"%@", [((FISDog *)dogThatCantWoof) assaultTheMailman]);
+    
+    
+    
+    //woof and assault
+    FISPet *secondPet = [[FISDog alloc] init];
+    
+    NSLog(@"The dog still goes: %@", [secondPet makeASound]);
+    
+    NSLog(@"The Mailman says: %@", [((FISDog *)secondPet) assaultTheMailman]);
+    
+
+
+    //pet me
+    FISPet *thirdPet = [[FISPet alloc] init];
+    
+    FISDog *dogThatCantWoof = (FISDog *)thirdPet;
+    
+    NSLog(@"The dog NOW goes: %@",[dogThatCantWoof makeASound]);
+    
+    //NSLog(@"The mailman says: %@",[((FISDog *)newPet) assaultTheMailman]);
+           //will compile but not run
+    
     return YES;
 }
 
